f9c1dd9c0dd1b4db7847988484366723ba0240c9
maanex <andreas@maanex.me>
2021-08-04 10:37:37 +0200

    LAST COMMIT BEFORE THE PURGE

--- src/modules/dailytopic.ts
+++ src/modules/dailytopic.ts
@@ -59,3 +59,263 @@ export default class DailyTopicModule extends Module {
   }
 
 }
+
+
+/*
+
+::root
+who is (user) and why do they (action)
+why does (user) (action)
+why do people (action)
+who would win in a (fight_name), (user) or (user)?
+who looks the most like (person)?
+(user) (kinda) reminds me of (person)
+(user) (kinda) reminds me of (person), don't you think?
+(user) (kinda) reminds me of (person)... or is it just me?
+(user)
+(person)
+(things)
+Frank
+Today we only talk about (user). If anyone tries to switch the topic they'll get banned!
+(what_is) the (adjective) (thing)?
+(what_is) the (adjective) (thing) you know of?
+(what_is) your faviourite (thing)?
+who would be the best couple in this server?
+share your (adjective) (shareables)
+share the (adjective) (shareables)
+what would you do without (things)?
+it's your last day on earth, what do you do?
+[redacted]
+(how_tos) (doable) while being (personal_condition)
+(how_tos) (doable) while being (personal_condition)?
+
+(emoji)
+(emoji_with_name)
+(x fact_about_today)
+(urban_dictionary_word_with_description)
+(random_wikihow_article)
+(reddit_news_headline)
+(random_meme_from_reddit)
+WTF is that? (weird_amazon_article)
+
+---
+
+::what_is
+what's
+what is
+
+::you_are
+you're
+you are
+
+::it_is
+it is
+it's
+
+::kinda
+kinda
+kind of
+sort of
+
+---
+
+::how_tos
+how to
+how do you
+how does one
+
+---
+
+::adjective
+best
+worst
+cutest
+weirdest
+strangest
+coolest
+most interesting
+least interesting
+most boring
+
+::shareables
+cat images
+dog images
+animal images
+memes
+wholesome memes
+quotes
+screenshots
+selfies
+recepies
+wikihow articles
+(things)
+
+::fight_name
+fist fight
+box fight
+gun fight
+sword fight
+fight for life and death
+fight for honor
+league of legends 1v1
+cs:go 1v1
+
+::action
+like (things)?
+not like (things)?
+hate (things)?
+love (things)?
+look so... (mood) today?
+
+::mood
+happy
+sad
+burnt out
+depressed
+lonely
+great
+awesome
+
+::thing
+game
+car
+computer game
+board game
+kind of sport
+website
+food
+youtube video
+series
+movie
+social media
+book
+piece of clothing
+animal
+country
+compliment
+way to (doable)
+
+::things
+(person)
+(user)
+games
+cars
+books
+movies
+alcohol
+friends
+any money
+social media
+
+::person
+Elon Musk
+Floyd Mayweather
+Katy Perry
+Howard Stern
+Garth Brooks
+James Patterson
+Robert Downey Jr.
+Taylor Swift
+Rush Limbaugh
+Ellen DeGeneres
+Lionel Messi
+Dr. Phil McGraw
+Calvin Harris
+LeBron James
+Justin Timberlake
+David Copperfield
+Sean Combs
+Ryan Seacrest
+Lady Gaga
+Jay Z
+Beyonce Knowles
+Kevin Durant
+Toby Keith
+Kim Kardashian
+Jennifer Lawrence
+Paul McCartney
+Phil Mickelson
+Tiger Woods
+Kobe Bryant
+Ben Roethlisberger
+Vin Diesel
+Judy Sheindlin
+Jason Aldean
+Luke Bryan
+Kenny Chesney
+Bradley Cooper
+Adam Sandler
+Tom Cruise
+Bruno Mars
+Drake
+Tim McGraw
+Florida Georgia Line
+Jimmy Buffett
+Jerry Seinfeld
+Scarlett Johansson
+David Letterman
+Jon Lester
+Derrick Rose
+Maroon 5
+Dr. Dre
+Zac Brown Band
+Mark Wahlberg
+Pharrell Williams
+Dwayne Johnson
+Eminem
+Britney Spears
+Carmelo Anthony
+Johnny Depp
+Maria Sharapova
+Leonardo DiCaprio
+Sean Hannity
+Carson Palmer
+Jim Parsons
+Channing Tatum
+Kevin Hart
+Miranda Lambert
+Jennifer Lopez
+
+::doable
+make a mess
+make money
+make a lot of money
+trade stocks
+waste time
+work out
+download the shrek movie
+survive a monkey attack
+stalk (user)
+pet a lion
+escape depression
+multiply two numbers
+beat box
+meditate
+become famous
+break up with your partner
+survive an ancounter with an ostrich
+dance
+get into a romantic relationship
+become a doctor
+act normal
+win a checkpot
+make friends
+
+::personal_condition
+trapped in a basement
+in online classes
+on a date
+chased by (user)
+chased by (person)
+chased by police
+asked for directions
+in a hospital
+grocery shopping
+high
+super thirsty
+very tired
+as lazy as possible
+in church
+on an interview in national news
+
+*/
