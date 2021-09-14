4186aba2da906d94bca80b5c033526fd564a2517
maanex <andreas@maanex.me>
2021-09-14 15:56:16 +0200

    docker extras

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -2,22 +2,26 @@
 /* eslint-disable no-eval */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { User, TextChannel } from 'discord.js'
+import { Docker } from 'node-docker-api'
 import { TudeBot } from '..'
 import { Command, CommandExecEvent, ReplyFunction } from '../types/types'
 
 
 export default class EvalCommand extends Command {
 
+  private static docker = new Docker({ socketPath: '/var/run/docker.sock' })
+
   constructor() {
     super({
       name: 'eval',
       description: 'Eval',
       sudoOnly: true,
+      aliases: [ 'evalr', '>', '>>' ],
       groups: [ 'internal' ]
     })
   }
 
-  public execute(channel: TextChannel, user: User, args: string[], event: CommandExecEvent, repl: ReplyFunction): boolean {
+  public async execute(channel: TextChannel, user: User, args: string[], event: CommandExecEvent, repl: ReplyFunction): Promise<boolean> {
     if (user.id !== '137258778092503042') return false
 
     try {
@@ -30,8 +34,12 @@ export default class EvalCommand extends Command {
       const bot = TudeBot.user
       const self = TudeBot.user
       const core = TudeBot.user
+      const docker = EvalCommand.docker
 
-      channel.send(`\`\`\`${eval(args.join(' '))}\`\`\``)
+      const comm = (/((>>)|r)$/.test(event.label) ? 'return ' : '') + args.join(' ')
+      const built = `(async () => { ${comm} })()`
+      const res = await eval(built)
+      channel.send(`\`\`\`${res}\`\`\``)
       return true
     } catch (ex) {
       repl('Error:', 'message', '```' + ex + '```')
