ceb64ba53c35e5e2488eabe0a493a59304855b34
maanex <andreas@maanex.me>
2021-10-03 17:05:12 +0200

    experiments3

--- src/modules/thebrain/link-analyzer.ts
+++ src/modules/thebrain/link-analyzer.ts
@@ -1,15 +1,14 @@
 import { Message } from 'discord.js'
 import * as ogs from 'open-graph-scraper'
-import * as getUrls from 'get-urls'
 
 
 export default class LinkAnalyzer {
 
-  public static rawMessage(message: Message) {
-    if (!message.content.includes('.')) return
-    const urls = getUrls(message.content)
-    for (const link of urls)
-      this.analyze(link)
+  public static rawMessage(_message: Message) {
+    // if (!message.content.includes('.')) return
+    // const urls = getUrls(message.content)
+    // for (const link of urls)
+    //   this.analyze(link)
 
   }
 
