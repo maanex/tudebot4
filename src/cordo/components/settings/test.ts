3be893bf519bf9f4a9bcc21b47fed9058f9400c0
maanex <andreas@maanex.me>
2022-04-02 17:40:15 +0200

    mmm2

--- src/cordo/components/settings/test.ts
+++ src/cordo/components/settings/test.ts
@@ -6,14 +6,14 @@ export default function (i: ReplyableComponentInteraction) {
     content: 'yikes',
     embeds: [],
     components: [
-      {
-        type: ComponentType.CHANNEL_SELECT,
-        custom_id: 'something',
-        placeholder: 'Gaming',
-        channel_types: [ ChannelType.GUILD_TEXT, ChannelType.GUILD_NEWS ],
-        min_values: 0,
-        max_values: 1
-      },
+      // {
+      //   type: ComponentType.CHANNEL_SELECT,
+      //   custom_id: 'something',
+      //   placeholder: 'Gaming',
+      //   channel_types: [ ChannelType.GUILD_TEXT, ChannelType.GUILD_NEWS ],
+      //   min_values: 0,
+      //   max_values: 1
+      // },
       {
         type: ComponentType.BUTTON,
         style: ButtonStyle.SECONDARY,
